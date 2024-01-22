e7ef9b80afe768ea4fb62c4d5bd5c881f55feed5
Ayushi0903 <ayushi.nandeshwar@vista.com>
2024-01-22 17:15:22 +0530

    updated terraform script

--- terraform/frontend/main.tf
+++ terraform/frontend/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
-resource "aws_ecr_repository" "tax_calc_proxy" {
+data "aws_ecr_repository" "tax_calc_proxy" {
   name = "tax-calc-proxy"
 }
 
@@ -15,27 +15,23 @@ resource "aws_ecs_task_definition" "frontend_task" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
-  container_definitions = <<DEFINITION
-[
-  {
-    "name": "frontend-container",
-    "image": "${aws_ecr_repository.tax_calc_proxy.repository_url}:latest",  
-    "cpu": 256,
-    "memory": 512,
-    "essential": true,
-  }
-]
-DEFINITION
+  container_definitions = jsonencode([{
+    name      = "frontend-container",
+    image     = "${data.aws_ecr_repository.tax_calc_proxy.repository_url}:latest",
+    cpu       = 256,
+    memory    = 512,
+    essential = true,
+  }])
 }
 
 resource "aws_ecs_service" "frontend_service" {
-  name            = "-service"
+  name            = "frontend-service"
   cluster         = aws_ecs_cluster.frontend_cluster.id
   task_definition = aws_ecs_task_definition.frontend_task.arn
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = ["subnet-0003d5ada12f964ca"] 
-    security_groups = ["sg-0737d50b02c59bbad"]      
+    subnets         = ["subnet-0003d5ada12f964ca"]
+    security_groups = ["sg-0737d50b02c59bbad"]
   }
 }
